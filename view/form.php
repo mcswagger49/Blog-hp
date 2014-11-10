94df9d6a630ac39c33389839c0b90149952dbed2
Joshua Smith <joshsmith0419@gmail.com>
2014-11-10 10:45:00 -0800

    Video/22 Milestone 11/10/14

--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1>Create Blog Post</h1><!-- Header for the blog post -->
 	
-<form>
+<form method="post" action="<?php echo $path ."controller/create-post.php"; ?>"> 
 	<div>
 		<label for="title">Title: </label><!-- labels the title -->
 		<input type="text" name="title" />
