d228f61e397e02d8144e5eb47869a98514ed7ead
Joshua Smith <joshsmith0419@gmail.com>
2014-11-13 08:57:45 -0800

    fixed comment in my query

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,12 +5,12 @@ $connection = new mysqli($host, $username, $password, $database);//use the infom
  $title = filter_input(INPUT_POST,"title", FILTER_SANITIZE_STRING);#filtering our input to the title
  $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);#filtering the input from post
 
- $query = $connection->query("INSERT INTO posts SET Title = '$title', post = '$post'");
+ $query = $connection->query("INSERT INTO posts SET Title = '$title', post = '$post'");//making it tru or false to run on the webpage
  
  if ($query) {
     echo "<p>Successfully inserted post: $title</p>";//echos out the post onto the page
  } else {
- 	echo "<p>connection->error</p>";//fixes the connection
+ 	echo "<p>connection->error</p>";//fixes the connection to not error
  }
  
 $connection->close();//closes the connection 
