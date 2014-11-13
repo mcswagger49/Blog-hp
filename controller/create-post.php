81bcca9347ff518e430dc83f33bda9a7c36deea8
Joshua Smith <joshsmith0419@gmail.com>
2014-11-13 08:52:29 -0800

    Successfully inserted a post on the page

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,7 +5,12 @@ $connection = new mysqli($host, $username, $password, $database);//use the infom
  $title = filter_input(INPUT_POST,"title", FILTER_SANITIZE_STRING);#filtering our input to the title
  $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);#filtering the input from post
 
- echo "<p>Title: $title</p>";#echos out the Title 
- echo "<p>Post: $post</p>";#echos out the posts on the page 
-
+ $query = $connection->query("INSERT INTO posts SET Title = '$title', post = '$post'");
+ 
+ if ($query) {
+    echo "<p>Successfully inserted post: $title</p>";//echos out the post onto the page
+ } else {
+ 	echo "<p>connection->error</p>";//fixes the connection
+ }
+ 
 $connection->close();//closes the connection 
