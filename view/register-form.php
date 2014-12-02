9f2ca4c16c2a2731e01bd799470f1f7913861726
Joshua Smith <joshsmith0419@gmail.com>
2014-12-02 08:53:05 -0800

    creating the register form

---
+++ view/register-form.php
@@ -0,0 +1,19 @@
+<form>
+      <div>
+          <label for="email">Email: </label><!--show the email label -->
+          <input type="text" name="email" /><!-- allows user to type text -->
+      </div>
+
+      <div>
+      	  <label for="username">Username: </label><!--show the username label tag -->
+      	  <input type="text" name="username" /><!-- allows user to type text -->
+      </div>
+      <div>
+        	<label for="password">Password: </label><!--show the password label -->
+        	<input type="password" name="password" /><!-- allows user to type password -->
+      </div>
+
+      <div>
+      	   <button type="submit">Submit</button><!-- the submit button on the website -->
+      </div>
+</form>
