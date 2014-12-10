4eb1c429e3ca9e6d209de180545895abfd2b3b07
Joshua Smith <joshsmith0419@gmail.com>
2014-12-10 09:18:02 -0800

    made site to readmore

--- view/header.php
+++ view/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html  lang="en" class="no-js">
 	<head>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -18,7 +18,7 @@
   </style>
 	<title></title>
 </head>
-<body>
+<body class="home blog">
 
 
 
--- view/navigation.php
+++ view/navigation.php
@@ -1,9 +1,17 @@
 <?php
 require_once(__DIR__ . "/../model/config.php");// requires the new file config.php 
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<div class="page-wrap"></div>
 <nav><!-- one nav bar on the top of the page -->
 <ul>
 	<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li><!--points to post file--> 
 </ul>
 </nav>
+</head>
+<body>
+
+</body>
+</html>
