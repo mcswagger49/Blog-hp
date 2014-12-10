4e9fa34ed5b99845670ee6719f5a1ed54d440ac3
Joshua Smith <joshsmith0419@gmail.com>
2014-12-09 21:36:50 -0800

    all fixed code to  go with "readmore"

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<html  lang="en" class="no-js">
+	<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+  	<link rel="stylesheet"  media="screen and (min-width: 0px)" href="css/small.css">
+    <link rel="stylesheet"  media="screen and (min-width: 1000px)" href="css/medium.css">
+    <link rel="stylesheet"  media="screen and (min-width: 1400px)" href="css/large.css">
+    <meta name="viewport" content="width=device-width, initial-scale1.0">
+    <style media="screen">
+    body { font: 18px/1.5 'Ubuntu Condensed', sans-serif; color: black; }
+    code { color: black; font-family: 'Ubuntu Condensed', sans-serif;}
+    a { color: #178DB1; }
+    .container { margin: 0 auto; max-width: 960px; }
+    #info + .readmore-js-toggle { padding-bottom: 1.5em; border-bottom: 1px solid #999; font-weight: bold;}
+    #demo { padding: 0 10%; }
+  </style>
+	</head>
+	<body>
 
 <?php
 	require_once(__DIR__ . "/view/header.php");//requires the header
@@ -6,3 +25,21 @@
 	require_once(__DIR__ . "/view/footer.php");//requires the footer
 	require_once(__DIR__ . "/controller/read-post.php");
 ?>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.1.1/jquery.min.js"></script>
+
+ <script src="readmore.js"></script>
+
+ <script>
+  $('#info').readmore({
+    moreLink: '<a href="#">More examples and options</a>',
+    maxHeight: 390,
+    afterToggle: function(trigger, element, expanded) {
+      if(! expanded) { // The "Close" link was clicked
+        $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
+      }
+    }
+  });
+    $('article').readmore({maxHeight: 240});
+  </script>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+  	<link rel="stylesheet"  media="screen and (min-width: 0px)" href="css/small.css">
+    <link rel="stylesheet"  media="screen and (min-width: 1000px)" href="css/medium.css">
+    <link rel="stylesheet"  media="screen and (min-width: 1400px)" href="css/large.css">
+    <meta name="viewport" content="width=device-width, initial-scale1.0">
+  <style media="screen">
+    body { font: 18px/1.5 'Ubuntu Condensed', sans-serif; color: black; }
+    code { color: black; font-family: 'Ubuntu Condensed', sans-serif;}
+    a { color: #F75814; }
+    .container { margin: 0 auto; max-width: 960px; }
+    #info + .readmore-js-toggle { padding-bottom: 1.5em; border-bottom: 1px solid #999; font-weight: bold;}
+    #demo { padding: 0 10%; }
+  </style>
+	<title></title>
+</head>
+<body>
+
+
+
+			
