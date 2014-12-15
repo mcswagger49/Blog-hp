fad4dd80eebd9cee4a6ace4aac8be8d5be0648db
Joshua Smith <joshsmith0419@gmail.com>
2014-12-14 19:12:42 -0800

    img on blog

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,6 @@
+<article>
+		<img width="200" height="200" align="right"src="http://www.laboiteverte.fr/wp-content/uploads/2014/03/gif-hypnotique-024.gif">
+</article>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="js/readmore.min.js"></script>
 
